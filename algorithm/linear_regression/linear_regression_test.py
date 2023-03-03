447220686ba0b622dd7d714a16bab2131e3afd5a
keane <jinxiaoming215@aliyun.com>
2023-03-03 10:21:12 +0800

    简单的线性回归:y = kx

--- algorithm/linear_regression/linear_regression_test.py
+++ algorithm/linear_regression/linear_regression_test.py
@@ -28,10 +28,32 @@ def load_data(file_path: str) -> list:
     return data
 
 
+def liner_regression(x, y):
+    """
+    简单线性回归公式：
+            y = kx   y:为预测值  y^为实际值
+    实际值与误测值之间存在误差，找到合适的公式（即使得误差最小的公式），误差和(&)：最小
+            &平方= （y^-kx）的平方
+    误差和最小，(y^-kx)的平方的最低点，对（y^-kx）的平方进行求导，得到关于参数k的公式，导数为0的点即为最小值点
+    0 = -2xy^+2kx**   k = xy/x**  除法为乘以该矩阵的逆矩阵
+    :return:
+    """
+    # x的平方，即x的转置乘以x,
+    x_square = x.T.dot(x)
+    # 判断矩阵是否可逆，判断此矩阵的行列式值是否为0，如果不是0，可逆
+
+    det_val = np.linalg.det(x_square)  # 该矩阵行列式的值
+    if det_val ==0.0:
+        raise ValueError(f"x矩阵不可逆")
+    # 求逆矩阵（求x_square的逆矩阵）
+    inverse = np.linalg.inv(x_square)
+
+    w = inverse.dot(x.T.dot(y.T))
+    print(w)
+
+
 data = load_data(DATA_PATH)
 np_data = np.array(data)
-x = np_data[:,0:2]
-y = np_data[:,-1]
-plt.scatter(x,y)
-plt.show()
-
+x = np_data[:, 0:2]
+y = np_data[:, -1]
+liner_regression(x,y)
