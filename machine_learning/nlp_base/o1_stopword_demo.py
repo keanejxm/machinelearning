600f157983a3039653702243b2861a7989b8ee69
keane <jinxiaoming215@aliyun.com>
2023-11-09 17:55:51 +0800

    fruitsd

---
+++ machine_learning/nlp_base/o1_stopword_demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+"""
+:author: keane
+:file  o1_stopword_demo.py
+:time  2023/11/9 16:06
+:desc  
+"""
+import jieba
+
+def get_stopword_list():
+    stop_word_path = "stopword.txt"
+    with open(stop_word_path, "r", encoding="utf8") as f:
+        lines = f.readlines()
+        stopword_list = [line.strip() for line in lines]
+        return stopword_list
+
+
+print(get_stopword_list())
