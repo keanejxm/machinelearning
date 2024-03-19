4d8b91b186287c80d61d0a8f9d2aa530fbe016a2
keane <jinxiaoming215@aliyun.com>
2024-03-19 18:01:52 +0800

    图像

--- 深度学习/图像处理/opencv练习/01_图像灰度化.py
+++ 深度学习/图像处理/opencv练习/01_图像灰度化.py
@@ -7,14 +7,15 @@
 # @Software: PyCharm
 
 import cv2
-from 深度学习.图像处理.opencv练习.img_config import img_path,opencv_data_path
-img = cv2.imread(f"{img_path}/lena.jpg",flags=1)# 1:彩色 0：灰度
-cv2.imshow("img",img)
+from 深度学习.图像处理.opencv练习.img_config import img_path, opencv_data_path
+
+img = cv2.imread(f"{img_path}/lena.jpg", flags=1)  # 1:彩色 0：灰度
+cv2.imshow("img", img)
 
 # 彩色---->灰度  BGR--->GRAY
-gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("gray",gray)
+cv2.imshow("gray", gray)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
--- 深度学习/图像处理/图像分类/image_practice.py
+++ 深度学习/图像处理/图像分类/image_practice.py
@@ -61,3 +61,4 @@ import numpy as np
 
 a = np.zeros([3, 4])
 print(a)
+
