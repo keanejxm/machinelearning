6338fdf2b1751eb23943f7136a333d77bdf8aad6
keane <1197523286@qq.com>
2024-07-01 09:48:29 +0800

    opencv

--- 机器学习/聚类/聚类_DBSCAN聚类/dbscan_demo_01.py
+++ 机器学习/聚类/聚类_DBSCAN聚类/dbscan_demo_01.py
@@ -5,3 +5,50 @@
 # @Site    : 
 # @File    : dbscan_demo_01.py
 # @Software: PyCharm
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.cluster as sc
+
+
+class ClusterDbscan:
+    def __init__(self):
+        self.data_path = r"D:\keane_data\data_test\multiple3.txt"
+
+    # 读取数据
+    def read_data(self):
+        """"""
+        data = pd.read_csv(self.data_path, names=["x1", "x2"])
+        return data
+
+    # 创建模型
+    @staticmethod
+    def dbscan_model(data):
+        """
+        构建DBSCAN聚类模型
+        Returns:
+
+        """
+        model = sc.DBSCAN(
+            eps=0.5,  # 圆的半径
+            min_samples=5,  # 最小样本数
+        )
+        model.fit(data)
+        label = model.labels_
+        # print(label)
+        return label
+
+    def plt_draw(self,data,label):
+        """"""
+        plt.figure("DBSCAN_Cluster")
+        plt.scatter(data["x1"],data["x2"],c=label)
+        plt.show()
+
+    def start(self):
+        data = self.read_data()
+        label = self.dbscan_model(data)
+        self.plt_draw(data,label)
+
+
+if __name__ == '__main__':
+    dbscan_obj = ClusterDbscan()
+    dbscan_obj.start()
