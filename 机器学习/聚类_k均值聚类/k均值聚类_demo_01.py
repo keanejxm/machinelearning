420893ec8736014c1fcc216ef58596b6b2e61440
keane <1197523286@qq.com>
2024-07-01 09:29:40 +0800

    opencv

---
+++ 机器学习/聚类_k均值聚类/k均值聚类_demo_01.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/6/30 22:30
+# @Author  : keane
+# @Site    : 
+# @File    : 聚类_k均值聚类.py
+# @Software: PyCharm
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.cluster as sc
+
+
+class KMean:
+    def __init__(self):
+        self.data_path = r"D:\keane_data\data_test\multiple3.txt"
+
+    def read_data(self):
+        """
+        读取数据
+        Returns:
+
+        """
+        data = pd.read_csv(self.data_path, header=None, names=["x1", "x2"])
+        # print(data)
+        return data
+
+    @staticmethod
+    def k_mean_model(data):
+        model = sc.KMeans(n_clusters=4)
+        model.fit(data)
+        label = model.labels_  # 分类类别
+        centers = model.cluster_centers_
+        print(centers)
+        # print(label)
+        return label, centers
+
+    @staticmethod
+    def plt_draw(data, label, centers):
+        plt.figure("k_mean聚类")
+        plt.title("聚类分布")
+        plt.scatter(data["x1"], data["x2"], c=label)
+        centers_df = pd.DataFrame(centers, columns=["x1", "x2"])
+        plt.scatter(centers_df["x1"], centers_df["x2"], marker="+",linewidths=20)
+        plt.show()
+
+    def start(self):
+        data = self.read_data()
+        # self.plt_draw(data)
+        label, centers = k_mean_obj.k_mean_model(data)
+        self.plt_draw(data, label, centers)
+
+
+if __name__ == '__main__':
+    k_mean_obj = KMean()
+    k_mean_obj.start()
