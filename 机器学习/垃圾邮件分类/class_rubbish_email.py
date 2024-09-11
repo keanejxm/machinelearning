2dd6dd8c5df36e5519a21e7c90aab9baa20517df
keane <jinxiaoming215@aliyun.com>
2024-09-11 17:48:46 +0800

    plt

--- 机器学习/垃圾邮件分类/class_rubbish_email.py
+++ 机器学习/垃圾邮件分类/class_rubbish_email.py
@@ -6,14 +6,22 @@
 # @File    : class_rubbish_email.py
 # @Software: PyCharm
 import os
+import re
+
 import jieba
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import sklearn.naive_bayes as nb
+import sklearn.metrics as sm
 
 
 class ClassRubbishEmail:
     def __init__(self):
-        self.nlp_dir = r"D:\keane_data\02_nlp_data"
+        if os.path.exists(r"D:\keane_data\02_nlp_data"):
+            self.nlp_dir = r"D:\keane_data\02_nlp_data"
+        else:
+            self.nlp_dir = r"E:\keane_data\nlp_data"
         self.rubbish_dir = os.path.join(self.nlp_dir, "rubbish_email_data")
         self.ham_path = os.path.join(self.rubbish_dir, "ham_data.txt")
         self.spam_path = os.path.join(self.rubbish_dir, "spam_data.txt")
@@ -44,52 +52,67 @@ class ClassRubbishEmail:
 
         return corpus, labels
 
-    def clear_space_data(self, corpus, labels):
+    # 删掉空数据
+    def clear_data(self, corpus, labels):
         filter_corpus = list()
         filter_label = list()
         for data, label in zip(corpus, labels):
-            if data:
-                filter_corpus.append(data)
-                filter_label.append(label)
+            # 删除掉空数据
+            if not data:
+                continue
+            # 对数据进行处理去掉标点符号和停用词
+
+            # 分词
+            data_cut: list = jieba.lcut(data)
+            # 删除标点符号
+            data_symbol = self.filter_symbol(data_cut)
+            # 去除停用词
+            data_stopword: list = [word for word in data_symbol if word not in self.stopwords_list]
+            new_data = "".join(data_stopword)
+            filter_corpus.append(new_data)
+            filter_label.append(label)
         return filter_corpus, filter_label
 
-    def participle(self, corpus, labels):
-        cut_corpus = list()
-        cut_labels = list()
-        for data, label in zip(corpus, labels):
-            cut_data = jieba.lcut(data)
-            # 去除停用词
-            cut_data = [word for word in cut_data if word not in self.stopwords_list]
-            cut_corpus.append(cut_data)
-            cut_labels.append(label)
-        return cut_corpus, cut_labels
-
-    def filter_symbol(self, corpus, labels):
+    @staticmethod
+    def filter_symbol(datas: list):
         """"""
-        new_corpus = list()
-        new_labels = list()
-        pun_list = [pun for pun in string.punctuation]
-        for data, label in zip(corpus, labels):
-            data = "".join([word for word in data if word not in pun_list])
-            new_corpus.append(data)
-            new_labels.append(label)
-        return new_corpus, new_labels
+        puns = string.punctuation
+        pattern = re.escape(puns)
+        new_data = [re.sub(pattern, "", data) for data in datas]
+        return new_data
 
-    def tf_idf_method(self, corpus, labels):
-        tf_idf = TfidfVectorizer(min_df=1,norm='l2',smooth_idf=True,use_idf=True)
-        tf_idf.fit(corpus)
-        tf_idf.transform(corpus)
+    def tf_idf_method(self, corpus):
+        vector = TfidfVectorizer(min_df=1, norm='l2', smooth_idf=True, use_idf=True)
+        tf_idf_res = vector.fit_transform(corpus)
+        return tf_idf_res, vector
+
+    # 构建模型
+    @staticmethod
+    def train_model(x, y):
+        """"""
+        model = nb.MultinomialNB()
+        model.fit(x, y)
+        return model
 
     def start(self):
+        """
+        1.读取数据
+        2.清洗数据：去掉空数据，去掉标点符号，去掉停用词
+        :return:
+        """
         corpus, labels = self.read_email()
-        # 对邮件进行处理，去除空数据
-        corpus, labels = self.clear_space_data(corpus, labels)
-        # 对数据进行分词
-        corpus, labels = self.participle(corpus, labels)
-        # 去除符号
-        corpus, labels = self.filter_symbol(corpus, labels)
-        # 去除停用词
-        print(corpus, labels)
+        # 对邮件数据进行清洗
+        corpus, labels = self.clear_data(corpus, labels)
+        # 训练集，测试集
+        train_x, test_x, train_y, test_y = train_test_split(corpus, labels, test_size=0.1)
+        # tfidf处理文字
+        train_x, vector = self.tf_idf_method(train_x)
+        model = self.train_model(train_x, train_y)
+        test_x = vector.transform(test_x)
+        pred_y = model.predict(test_x)
+        # 模型评估
+        res_sm = sm.classification_report(y_true=test_y, y_pred=pred_y)
+        print(res_sm)
 
 
 if __name__ == '__main__':
